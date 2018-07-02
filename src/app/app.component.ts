f3362a04fff24143c191be832d252fb6712cd559
Abhijeet Tiwari <abhijeet96tiwari@gmail.com>
2018-07-02 17:15:26 +0530

    Directives Assignment

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  displayDetails:any;
+  count:number = 1;
+  buttonCountArray = [];
+
+  toggleDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.buttonCountArray.push(this.count++);
+    
+  }
+
+  findIndex() {
+   return this.buttonCountArray.includes(5) === true ? 'green' : '' ;
+   // console.log(this.buttonCountArray.includes(5)); 
+ }
+
+}
